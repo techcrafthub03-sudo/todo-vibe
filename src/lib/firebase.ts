f862e6178d9f7ea07716aea928a0aa534124564d
techcrafthub03 <techcrafthub03@gmail.com>
2025-09-30 19:41:42 +0000

    .

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: "todo-vibe-7a533",
-  appId: "1:930166065949:web:10c9e15389bb306371aff4",
-  storageBucket: "todo-vibe-7a533.appspot.com",
-  apiKey: "AIzaSyDZUwHX7KahCiz4tu0AjrE6tVzpqSaa-Hg",
-  authDomain: "todo-vibe-7a533.firebaseapp.com",
-  messagingSenderId: "930166065949",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
